53c0c86fc04ef9c5c4f1dc2083a02171d50b98bb
jay-madane <jaymadane133@gmail.com>
2024-11-03 13:45:14 +0530

    Update dept classifiaction

--- department_classification.py
+++ department_classification.py
@@ -10,8 +10,12 @@ def dept_classifier(news_title):
     iniPrompt = ("You are a news department classifier for Indian news, take the news and output only the department without any text formatting, punctuations and use lowercase only. List of departments: sports, healthcare, business, politics, law, entertainment, weather, technology.")
     query = iniPrompt + news_title
     model = genai.GenerativeModel('gemini-1.5-flash')
-    response = model.generate_content(query)
-    return response.text
+    try:
+        response = model.generate_content(query)
+        return response.text
+    except Exception as e:
+        print(f"Error during classification: {e}")
+        return "controversial"
 
 # testing
 # print(dept_classifier("PM Modi arrives in Delhi after concluding two-nation visit to Russia a…"))
